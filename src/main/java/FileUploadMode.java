df97a0ac1791effd13dbf4388dcbb67040906a05
Guzman <francisco.guzman@accenture.com>
2024-08-28 16:49:14 -0400

    lab done

---
+++ src/main/java/FileUploadMode.java
@@ -0,0 +1,9 @@
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+public class FileUploadModel {
+    private String uploadedFile;
+}
